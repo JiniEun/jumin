b5699f8d3dcd9f8c9d57f79832a4c6f0c5e63727
serrimm <serim3464@gmail.com>
2021-11-09 13:37:40 +0900

    first commit

---
+++ src/main/java/com/dongne/jumin/JuminApplication.java
@@ -0,0 +1,13 @@
+package com.dongne.jumin;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class JuminApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(JuminApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/dongne/jumin/JuminApplicationTests.java
@@ -0,0 +1,13 @@
+package com.dongne.jumin;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class JuminApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
