b2226614ada86a203631b8fbd5017d74b5a92bfa
Jini-Eun <julietjin94@gmail.com>
2021-11-26 23:56:59 +0900

    Fix project setting

--- src/main/java/com/dongne/user/User.java
+++ src/main/java/com/dongne/user/User.java
@@ -16,7 +16,7 @@ public class User {
 		} else {
 			// System.out.println("Linux");
 			// 은진 - mac 경로
-			path = "/Users/jules/Study/AIstudy/deploy/dongne/user/storage/";
+			path = "/Users/AIstudy/deploy/dongne/user/storage/";
 		}
 
 		return path;
