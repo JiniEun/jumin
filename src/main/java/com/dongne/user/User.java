56492feacb701256a731f41a1b031536d0cfc71a
Jini-Eun <julietjin94@gmail.com>
2021-11-27 09:10:21 +0900

    Fix Mac route

--- src/main/java/com/dongne/user/User.java
+++ src/main/java/com/dongne/user/User.java
@@ -16,7 +16,7 @@ public class User {
 		} else {
 			// System.out.println("Linux");
 			// 은진 - mac 경로
-			path = "/Users/AIstudy/deploy/dongne/user/storage/";
+			path = "/Users/AIstudy/deploy/jumin/user/storage/";
 
 		}
 
